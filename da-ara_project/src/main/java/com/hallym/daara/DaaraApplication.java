e011dbde8f03f3cc14596b079b64662822bee1dd
gwonbe <syj021231@naver.com>
2024-03-30 16:07:32 +0900

    vite 요청 허용 코드 작성, proxy 설정

--- da-ara_project/src/main/java/com/hallym/daara/DaaraApplication.java
+++ da-ara_project/src/main/java/com/hallym/daara/DaaraApplication.java
@@ -2,12 +2,29 @@ package com.hallym.daara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DaaraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaaraApplication.class, args);
+	}
+
+	// Cross-Origin Resource Sharing (CORS)
+	// http://localhost:5173/ 으로 부터 오는 모든 요청 허용하기
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:5173");
+			}
+		};
 	}
 
 }
--- da-ara_project/src/main/java/com/hallym/daara/controller/DaaraController.java
+++ da-ara_project/src/main/java/com/hallym/daara/controller/DaaraController.java
@@ -1,15 +1,29 @@
 package com.hallym.daara.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class DaaraController {
+@Configuration
+public class DaaraController implements WebMvcConfigurer{
     
+    // vite 접속 허용
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
+    }
+
+    // http://localhost:8080/daara
     @RequestMapping("daara")
     public String requestMethodName() {
+        System.out.println("# [DaaraController] daara()");
         return "다아라에게 무엇이든 물어보세요!";
     }
     
-
 }
