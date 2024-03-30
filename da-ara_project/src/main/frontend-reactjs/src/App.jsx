5da18375b4265daee14f92f099193f95885d6d49
yerin <yerin6428@naver.com>
2024-03-30 14:44:02 +0900

    App, main jsx 생성

---
+++ da-ara_project/src/main/frontend-reactjs/src/App.jsx
@@ -0,0 +1,53 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/Login/LoginPage";
+import MainPage from "./pages/Chatting/MainPage";
+import SignupPage from "./pages/Login/SignupPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ChatPage from "./pages/Chatting/ChatPage";
+import Header from "./components/Header";
+
+const resize = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+const routerList = [
+  { path: "/", element: <MainPage /> },
+  { path: "/chatpage", element: <ChatPage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/signup", element: <SignupPage /> },
+  { path: "/header", element: <Header /> },
+];
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/daara")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    resize();
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {routerList.map((item, index) => (
+            <Route key={index} path={item.path} element={item.element} />
+          ))}
+        </Routes>
+      </Router>
+      받아온값 :{data}
+    </div>
+  );
+}
+
+export default App;
