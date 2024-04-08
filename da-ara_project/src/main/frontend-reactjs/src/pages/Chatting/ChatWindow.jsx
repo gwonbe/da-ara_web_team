89dbe355b368bae2ab72f5b02ac0e4cde62b7554
yerinJi <yerin6428@gmail.com>
2024-04-08 20:17:08 +0900

    버츄얼캐릭터 영상 삽입

--- da-ara_project/src/main/frontend-reactjs/src/pages/Chatting/ChatWindow.jsx
+++ da-ara_project/src/main/frontend-reactjs/src/pages/Chatting/ChatWindow.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import ChatMessage from "./ChatMessage";
 import ChatInput from "./ChatInput";
 
 const ChatWindow = () => {
   const [messages, setMassages] = useState([]);
+  const inputElem = useRef(null);
 
   const addMessage = (message, isUser) => {
     setMassages((prevMessages) => [...prevMessages, { text: message, isUser }]);
@@ -13,6 +14,7 @@ const ChatWindow = () => {
   const handleSubmit = async (message) => {
     //사용자 메시지를 추가합니다.
     addMessage(message, true);
+    inputElem.current.focus();
 
     try {
       // 백엔드 서버와 통신하여 GPT의 응답을 받습니다.
