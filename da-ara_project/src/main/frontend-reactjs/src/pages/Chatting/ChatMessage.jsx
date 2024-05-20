5700431f33143f9521ca4e14eaa3de619023ae66
yerinJi <yerin6428@gmail.com>
2024-05-20 11:37:12 +0900

    bot-messages margin 조정

--- da-ara_project/src/main/frontend-reactjs/src/pages/Chatting/ChatMessage.jsx
+++ da-ara_project/src/main/frontend-reactjs/src/pages/Chatting/ChatMessage.jsx
@@ -1,26 +1,21 @@
 const ChatMessage = ({ message, isUser }) => {
-  const messageStyle = {
-    marginBottom: isUser ? "16px" : "0",
-  };
-
   // // 문자열에서 줄바꿈을 처리하는 함수
   const formatMessage = (msg) => {
     console.log(typeof msg);
-    const parts = msg.split('<br>');
+    const parts = msg.split("<br>");
     return parts.map((part, index) => (
       <span key={index}>
         {part}
-        <br/>
+        <br />
       </span>
     ));
   };
-  
+
   return (
-    <div className={`chat-message ${isUser ? "user-message" : "bot-message"}`} style={messageStyle}>
+    <div className={`chat-message ${isUser ? "user-message" : "bot-message"}`}>
       <p>{formatMessage(message)}</p>
     </div>
   );
-
 };
 
 export default ChatMessage;
